a829c2edf729cf51ff70d9064c16baf1a804c80a
João Lucas <joaolucaspachecoab@gmail.com>
2022-07-19 09:34:55 -0300

    backend - Code improvements

--- voxy-test-backend/controllers/counter.js
+++ voxy-test-backend/controllers/counter.js
@@ -1,3 +1,8 @@
+/**
+ * Function that counts the number of words in a String
+ * @param {String} text - Text that should have its words counted
+ * @returns Returns the number of words in the text
+ */
 async function countWords(text) {
     try {
         const wordCount = text.match(/[^\s]+/g)
