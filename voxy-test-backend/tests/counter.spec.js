38e8de9c66484515370a474ae85e5b2d9215351f
João Lucas <joaolucaspachecoab@gmail.com>
2022-07-19 17:28:05 -0300

    backend - Code improvements

--- voxy-test-backend/tests/counter.spec.js
+++ voxy-test-backend/tests/counter.spec.js
@@ -3,39 +3,40 @@ const counterController = require('../controllers/counter')
 const chai = require('chai')
 const expect = chai.expect
 
-describe('Test /counter', ( ) => {
+describe('Testing /counter/count-words', ( ) => {
 
-    describe('Testing word counting on /count-words', ()=>{
+    context('when processing three words', () => {
+        it('should count and return 3', async () => {
+            const result = await counterController.countWords("We are three.")
+            expect(result).to.equal(3)
+        })
+    } )
+    
+    context('when processing an empty string', () => {
+        it('should count and return 0', async () => {
+            const result = await counterController.countWords("")
+            expect(result).to.equal(0)
+        })
+    } )
 
-        describe('Testing default case', () => {
-            it('Should count and return 3', async () => {
-                const result = await counterController.countWords("We are three.")
-                expect(result).to.equal(3)
-            })
-        } )
-        describe('Testing empty string', () => {
-            it('Should count and return 0', async () => {
-                const result = await counterController.countWords("")
-                expect(result).to.equal(0)
-            })
-        } )
-        describe('Testing string of empty spaces', () => {
-            it('Should count and return 0', async () => {
-                const result = await counterController.countWords("       ")
-                expect(result).to.equal(0)
-            })
-        } )
-        describe('Testing wrong input type case - Number', () => {
-            it('Should return 0', async () => {
-                const result = await counterController.countWords(123)
-                expect(result).to.equal(0)
-            })
-        } )
-        describe('Testing wrong input type case - Object', () => {
-            it('Should return 0', async () => {
-                const result = await counterController.countWords({"text": "Text"})
-                expect(result).to.equal(0)
-            })
-        } )
-    })
+    context('when processing a string of empty spaces', () => {
+        it('should count and return 0', async () => {
+            const result = await counterController.countWords("       ")
+            expect(result).to.equal(0)
+        })
+    } )
+
+    context('when processing a number as input', () => {
+        it('should return 0', async () => {
+            const result = await counterController.countWords(123)
+            expect(result).to.equal(0)
+        })
+    } )
+
+    context('when processing an object as input', () => {
+        it('Should return 0', async () => {
+            const result = await counterController.countWords({text: "Text"})
+            expect(result).to.equal(0)
+        })
+    } )
 })
